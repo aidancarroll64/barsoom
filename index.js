540365fc41b7bed5684e091d7cecaba9e60d4c43
Aidan Carroll <40276779+aidancarroll64@users.noreply.github.com>
2024-08-09 09:30:37 +0000

    initial version

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
+try {
 const name = core.getInput('name');
 console.log(`Hello ${name}!`);
 core.setOutput('greeting', outputValue);
+} catch (error) {
+    core.setFailed(error.message);
+}
